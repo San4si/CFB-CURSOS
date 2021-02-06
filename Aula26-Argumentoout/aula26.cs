451241b3ded7fd2051457e67726696cc54a45f57
San4si <san_juq@hotmail.com>
2021-02-06 04:36:18 -0300

    estudando

--- Aula26-Argumentoout/aula26.cs
+++ Aula26-Argumentoout/aula26.cs
@@ -1,13 +1,18 @@
 using System;
-class aula25 {
-    static void Main(){
-        int num = 10;
-        dobrar(ref num);
-        Console.WriteLine(num);
-
-    }
-    static void dobrar(ref int valor) /*\Argumento out da a possibilidade do metodo retornar mais de um valor*/
+class aula26 {
+    static void Main()
     {
-        valor*=2;
+        int divid,divis,quoc,rest;
+        divid=10;
+        divis=3;
+        quoc=divide(divid,divis, out rest);
+        Console.WriteLine("{0}/{1}:quociente = {2} e resto={3}", divid,divis,quoc,rest);
+    }
+    static int divide (int dividendo, int divisor, out int resto) /*Argumento out da a possibilidade de ter mais de um return no metodo ou seja ter duas saida*/
+    {
+        int quociente;
+        quociente=dividendo/divisor;
+        resto=dividendo%divisor;
+        return quociente;
     }
 }
